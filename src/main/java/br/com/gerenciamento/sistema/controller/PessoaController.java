fd00d8a3f4a2bad04d3cab431d972d367a714451
Nitai Charan <nitaicharan@gmail.com>
2019-07-18 03:31:53 -0300

    task#167365614 - Melhoria na classe PessoaController
    Atualizando métodos na mudança da anotação utilizada. Trocando a anotação RequestMapping para tipo de requisição específica (GetMapping, PostMapping, PutMapping, ...)

--- src/main/java/br/com/gerenciamento/sistema/controller/PessoaController.java
+++ src/main/java/br/com/gerenciamento/sistema/controller/PessoaController.java
@@ -18,15 +18,15 @@ public class PessoaController {
     @Autowired
     private PessoaService pService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<PessoaModel>>findAll(){
         return ResponseEntity.ok().body(pService.findAll());
     }
 
-    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    @GetMapping(value = "/{id}")
     public ResponseEntity <Void> delete(@PathVariable("id")Long id){ pService.delete(id); return ResponseEntity.noContent().build();}
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<PessoaModel> insert(@Valid @RequestBody PessoaModel p){
         pService.save(p);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(p.getId())
@@ -34,12 +34,12 @@ public class PessoaController {
         return ResponseEntity.created(uri).build();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public ResponseEntity<PessoaModel> findById(@PathVariable("id") Long id) throws Exception {
         return ResponseEntity.ok().body(pService.findById(id));
     }
 
-    @RequestMapping(value = "/status/inativos", method = RequestMethod.GET)
+    @GetMapping(value = "/status/inativos")
     public ResponseEntity<List<PessoaModel>> findInativo(){
         return ResponseEntity.ok().body(pService.findInativo());
     }
